b63a6c9446b144a18c54d22d60bd350b0b0da918
Olga <787558@ukr.net>
2020-10-05 20:54:39 +0300

    multiple choice

---
+++ src/main/java/com/company/homework/hw01/MultipleChoice.java
@@ -0,0 +1,10 @@
+package com.company.homework.hw01;
+
+public class MultipleChoice {
+    public static void main(String[] args) {
+        int correct = 10;
+        int incorrect = 15;
+        double finalScore = correct - 1.0/4*incorrect;
+        System.out.println(finalScore);
+    }
+}
