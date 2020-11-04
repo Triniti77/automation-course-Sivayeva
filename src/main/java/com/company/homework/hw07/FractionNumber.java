6f3c0520070195d206d36633c069b36bb4f81d2c
Olga <787558@ukr.net>
2020-11-04 16:06:39 +0200

    Добавила hashCode и equals в Homework07

--- src/main/java/com/company/homework/hw07/FractionNumber.java
+++ src/main/java/com/company/homework/hw07/FractionNumber.java
@@ -10,6 +10,31 @@ public class FractionNumber {
         this.bottom = bottom;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + top;
+        result = prime * result + bottom;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FractionNumber h = (FractionNumber) obj;
+        if (top != h.getTop())
+            return false;
+        if (bottom != h.getBottom())
+            return false;
+        return true;
+    }
+
     public FractionNumber plus(FractionNumber h){
         int top2 = h.getTop();
         int bottom2 = h.getBottom();
