92555311a05e54938609d3580b4628a37a80bd81
Olga <787558@ukr.net>
2020-11-11 16:52:10 +0200

    added class OrdersTestBase

--- src/main/java/com/company/lecture10/tests/CheckoutTests.java
+++ src/main/java/com/company/lecture10/tests/CheckoutTests.java
@@ -2,6 +2,12 @@ package com.company.lecture10.tests;
 
 import com.company.infrastructure.TestBase;
 
-public class CheckoutTests extends ClientTestBase {
+public class CheckoutTests extends OrdersTestBase {
 
+    @Override
+    protected void beforeTest() {
+        super.beforeTest();
+
+        logger.log("Click Checkout button");
+    }
 }
